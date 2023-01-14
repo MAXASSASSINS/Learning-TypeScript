1af36e42c165be94247bb195d74dfd0cecfb99fb
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-01-14 18:25:30 +0530

    lesson 2

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var character = 'mario';
+console.log(character);
+character = 'shadab';
+console.log(character);
+var temp = document.querySelectorAll('field');
+console.log(temp);
---
+++ sandbox.ts
@@ -0,0 +1,8 @@
+let character = 'mario';
+console.log(character);
+
+character = 'shadab';
+console.log(character);
+
+const temp = document.querySelectorAll('field')
+console.log(temp);
