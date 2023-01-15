f53cf5ea944599529c2bedd12b1abb4aa7bc007b
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-01-15 18:47:14 +0530

    lesson 6

--- sandbox.js
+++ sandbox.js
@@ -1,24 +1,6 @@
-// explicit types ---> used when we don't want to initialize the variable but we want to specify the type of the variable
-var character;
-var age;
-var isLoggedin;
-// age = 'shadab'; // error
-age = 13;
-// arrays
-var stringArray = []; // best to initialize with empty array
-stringArray = ['shadab', 'ali', 'ahmed'];
-// union types
-var mixedArray = []; // only need to put the type of the array in the parenthesis
-mixedArray.push('shadab');
-mixedArray.push(13);
-mixedArray.push(true);
-console.log(mixedArray);
-var id; // no need to put the type of the variable in the parenthesis
-// objects
-var shadab;
-shadab = { name: 'shadab', age: 20 };
-// shadab = ''; // error
-shadab = []; // no error as array is also an object
-var shadab2;
-// shadab2 = {name: 'shadab', age: 20}; // error as gamer is missing
-shadab2 = { name: 'shadab', age: 20, gamer: true };
+var age = 25;
+console.log(age);
+age = true;
+console.log(age);
+age = 'shadab';
+console.log(age);
--- sandbox.ts
+++ sandbox.ts
@@ -1,38 +1,11 @@
-// explicit types ---> used when we don't want to initialize the variable but we want to specify the type of the variable
-let character: string;
-let age: number;
-let isLoggedin: boolean;
+let age: any = 25;
 
-// age = 'shadab'; // error
-age = 13;
+console.log(age);
 
-// arrays
-let stringArray: string[] = []; // best to initialize with empty array
+age = true;
 
-stringArray = ['shadab', 'ali', 'ahmed'];
+console.log(age);
 
-// union types
-let mixedArray: (string | number | boolean)[] = []; // only need to put the type of the array in the parenthesis
-mixedArray.push('shadab');
-mixedArray.push(13);
-mixedArray.push(true);
+age = 'shadab';
 
-console.log(mixedArray);
-
-let id: string | number; // no need to put the type of the variable in the parenthesis
-
-
-// objects
-let shadab: object;
-shadab = { name: 'shadab', age: 20 }
-// shadab = ''; // error
-shadab = []; // no error as array is also an object
-
-let shadab2: {
-    name: string,
-    age: number,
-    gamer: boolean
-};
-
-// shadab2 = {name: 'shadab', age: 20}; // error as gamer is missing
-shadab2 = {name: 'shadab', age: 20, gamer: true};
+console.log(age);
